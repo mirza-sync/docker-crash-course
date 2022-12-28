dc5dbc708a2f68acd66328f82069de71a5d7ecd7
mirza-sync <mirzasyahmi123@gmail.com>
2022-12-29 00:07:00 +0800

    setup: nodemon

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 1234;
 app.get('/', async (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.status(200);
-  res.send('<h1>Hello World 123</h1>');
+  res.send('<h1>Hello World GG</h1>');
 });
 
 app.listen(port, () => {
