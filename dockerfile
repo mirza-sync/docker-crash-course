35ee37c745247ab8e8578499ff20a760c507126e
mirza-sync <mirzasyahmi123@gmail.com>
2023-01-01 20:48:59 +0800

    setup: change port number

--- dockerfile
+++ dockerfile
@@ -8,6 +8,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 1234
+EXPOSE 1000
 
 CMD ["npm", "run", "start"]
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1234;
+const port = 1000;
 
 app.get('/', async (req, res) => {
   res.setHeader('Content-Type', 'text/html');
